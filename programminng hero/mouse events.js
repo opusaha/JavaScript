392320c26261e6fc267b8f2fca1ee3b3595a8c5b
opusaha <sahaapo@gmail.com>
2022-03-08 16:22:59 +0600

    Mouse Event

---
+++ programminng hero/mouse events.js
@@ -0,0 +1,13 @@
+function mouseMove(){
+    console.log("Someone is moving.");
+  }
+  function mouseEnter() {
+    console.log("Some one is entered in here.");
+  }
+  function mouseOver() {
+    console.log("Someone is over in here.");
+
+
+
+    // html file:
+    // <button onmousemove="fncName()" onmouseenter="mouseEnter()" onmouseover="mouseOver()">Click me</button>
