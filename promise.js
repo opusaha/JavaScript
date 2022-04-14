76acf835ce1c92f8a0755802620cb4c4beae90f0
opusaha <sahaapo@gmail.com>
2022-04-14 12:32:27 +0600

    push

--- promise.js
+++ promise.js
@@ -1,9 +1,11 @@
 const p1 = new promise((resolve, reject)=>{
     let completedPromise = true;
-    if(completedPromise){
+    if(completedPromisen){
         resolve(`completed promise 1`)
     }else{
         reject(`not completed promise 1`)
     }
 });
-console.log(p1);
+p1.then((res)=>{
+    console.log(res);
+})
